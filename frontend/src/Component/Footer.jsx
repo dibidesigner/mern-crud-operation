4519fcc58a22cad79abc7a314a99d0fcc428843a
Dibi Hembram <dibihembram4@gmail.com>
2024-11-30 19:07:36 +0530

    ok

--- frontend/src/Component/Footer.jsx
+++ frontend/src/Component/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <>
         <div className='w-screen h-[556.26px] bg-[#222222] flex justify-center items-center'>
-           <label className='text-gradient-to-b from-[#36FF83] to-[#C92EC6]'>D I B I  H E M B R A M</label>
+           <label className='bg-gradient-to-b  bg-clip-text  from-[#36FF83] to-[#C92EC6] text-[40px] text-transparent'>D I B I  H E M B R A M</label>
            <div></div>
            <div></div>
         </div>
